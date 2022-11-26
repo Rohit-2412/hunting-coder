a489652846ce6d98a26a010d57575405e64aa47a
Rohit Kumar <rk212300@outlook.com>
2022-11-26 23:05:26 +0530

    Added Links

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Script from 'next/script'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,10 +13,10 @@ export default function Home() {
             </Head>
             <nav className={styles.mainnav}>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
+                    <Link href='/'><li>Home</li></Link>
+                    <Link href='/about'><li>About</li></Link>
+                    <Link href='/blog'><li>Blog</li></Link>
+                    <Link href='/contact'><li>Contact</li></Link>
                 </ul>
             </nav>
             <main className={styles.main}>
