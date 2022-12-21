6191d369736aeba074058d4906290cb0b54c9537
Rohit Kumar <rk212300@outlook.com>
2022-12-21 10:04:00 +0530

    Added Navbar

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,7 @@ export default function Home() {
                 <meta name="keywords" content="nextjs, huntingcoder blog, hunting coder" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <nav className={styles.mainnav}>
-                <ul>
-                    <Link href='/'><li>Home</li></Link>
-                    <Link href='/about'><li>About</li></Link>
-                    <Link href='/blog'><li>Blog</li></Link>
-                    <Link href='/contact'><li>Contact</li></Link>
-                </ul>
-            </nav>
+
             <main className={styles.main}>
                 <h1 className={styles.title}>
                     Hunting Coder
