41302201f102da172368cda4ec31d2af1a0f097f
Rohit Kumar <rk212300@outlook.com>
2022-12-21 20:55:40 +0530

    Added API Routes for Blogs

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,8 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-    fs.readdir("blogdata", (err, data) => {
-        res.status(200).json(data)
-    })
+export default async function handler(req, res) {
+    // first read the directory
+    let data = await fs.promises.readdir("blogdata");
+    // declare variable for holding the file
+    let myFile;
+    // array to store all the blogs
+    let allBlogs = [];
+
+    // traverse the array and read each file
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        // reading file
+        myFile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+        // pushing the file to the array
+        allBlogs.push(JSON.parse(myFile));
+    }
+
+    res.status(200).json(allBlogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 const Blog = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/blogs").then((a) => {
+            return a.json();
+        }).then((data) => {
+            setBlogs(data)
+        })
+    }, [])
     return (
         <div className="blogs">
             <main className={styles.main}>
+                {/* heading */}
                 <h1>Popular Blogs</h1>
-                <div className={styles.blogItem}>
-                    <Link href={'/blogpost/learn-javascript'}>
-                        <h2>How to learn JavaScript in 2022?</h2>
-                    </Link>
-                    <p>JavaScript is the language used to design logic for the web</p>
-                </div>
+
+                {/* using map function to display all the blogs */}
+                {blogs.map((blogitem) => {
+                    return <div key={blogitem.title}>
+                        <Link href={`/blogpost/${blogitem.slug}`}>
+                            <h2>{blogitem.title}</h2>
+                        </Link>
+                        <p className={styles.blogitemp}>
+                            {/* display the first 20 words and then ...*/}
+                            {blogitem.content.split(" ").slice(0, 20).join(" ")}...
+                        </p>
+                    </div>
+                })}
 
             </main>
         </div>
