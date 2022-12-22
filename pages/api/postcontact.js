6227e0e1172d274144095bb43515b80d4240a2d3
Rohit Kumar <rk212300@outlook.com>
2022-12-22 17:22:57 +0530

    Contact API

---
+++ pages/api/postcontact.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        let data = await fs.promises.readdir('contactdata')
+        fs.promises.writeFile(`contactdata/${data.length + 1}.json`, JSON.stringify(req.body))
+        res.status(200).json({ message: "success" })
+    }
+    else {
+        res.status(200).json(["allBlogs"])
+    }
+}
